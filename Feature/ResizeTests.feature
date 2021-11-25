fd658eed17c6ffbd988b7837575a6b384e63f1c7
Lucja <lucja.dybek@jcommerce.pl>
2021-11-25 11:49:19 +0100

    Alert tests

--- Feature/ResizeTests.feature
+++
@@ -1,7 +0,0 @@
-﻿Feature: ResizeTests
-	Simple calculator for adding two numbers
-
-@mytag
-Scenario: Add two numbers
-	When User resizes a window
-	#Then the result should be 120
--- StepDefinitions/AlertSteps.cs
+++ StepDefinitions/AlertSteps.cs
@@ -1,4 +1,5 @@
 ﻿using Atata;
+using FluentAssertions;
 using IFlow.Testing.Pages;
 using OpenQA.Selenium;
 using System;
@@ -9,19 +10,40 @@ namespace IFlow.Testing.StepDefinitions
 {
     [Binding]
     public sealed class AlertSteps : BaseSteps
-    {
-        [When(@"User click submit button alert appears")]
-        public void DeleteUsingJSConfirm()
+    {    
+        private AlertPage alertPage;
+
+        [When(@"User click submit button and alert appears")]
+        public void UserClickSubmitButtonAndAlertAppears()
         {
-            Go.To<AlertPage>()
+            alertPage = Go.To<AlertPage>()
                 .AlertButton.Click();
         }
 
-        [When(@"User click submit button and confirms alert that appears")]
-        public void ConfirmUsingJSConfirm()
+        [Then(@"Alert message is displayed")]
+        public void AlertMessageIsDisplayed()
         {
-            Go.To<AlertPage>()
+            alertPage.GetAlertMessage().Equals("You clicked a button").Should().BeTrue();
+        }
+
+        [Then(@"user confirms it with button")]
+        public void UserConfirmsItWithButton()
+        {
+            alertPage.AcceptAlert();
+        }
+
+        [When(@"User click submit button and confirms alert that appears")]
+        public void UserClickSubmitButtonAndConfirmsAlertThatAppears()
+        {
+            alertPage = Go.To<AlertPage>()
                 .Cancel.Click();
+            alertPage.AcceptAlert();
+        }
+
+        [Then(@"Correct message appears on page")]
+        public void CorrectMessageAppearsOnPage()
+        {
+            alertPage.ResultText.Value.Equals("You selected Ok").Should().BeTrue();
         }
     }
 }
