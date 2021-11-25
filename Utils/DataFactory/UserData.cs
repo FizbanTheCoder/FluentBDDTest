cf1c4efdc39ea5cfd31735d74f16e647ada1da04
Lucja <lucja.dybek@jcommerce.pl>
2021-11-25 11:56:22 +0100

    Api tests clean up

--- Utils/DataFactory/UserData.cs
+++ Utils/DataFactory/UserData.cs
@@ -6,7 +6,7 @@ namespace IFlow.Testing.Utils.DataFactory
 {
     public static class UserData
     {
-        public static readonly User CorrectApiUser = new User("68cb8daf20msh72afcd9c94f4186p1a4129jsn069149a72101", "x-rapidapi-key");
+        public static readonly User CorrectApiUser = new User(UserConsts.key, UserConsts.header);
 
         [Obsolete("Visual Studio IntelliSense Work Around", true)]
         public static Faker<User> CreateUserData()
